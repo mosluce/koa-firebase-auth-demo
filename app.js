30b0f356b0f19aa216e3167db64744b4f4c08879
mosluce <mosluce@gmail.com>
2017-05-25 14:58:59 +0800

    update module

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const logger = require('koa-logger');
 const path = require('path');
 
-const staticMiddleware = require('./libs/koa-static');
+const staticMiddleware = require('./libs/koa-static-middleware');
 const firebaseAuth = require('./libs/firebase-auth');
 const router = require('./router');
 
